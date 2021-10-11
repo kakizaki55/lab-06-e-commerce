ce01f214d15e6143f1d57a9f6a49fef5e017cff0
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-11 16:11:41 -0700

    going to do my first push

--- products.js
+++ products.js
@@ -1,34 +1,44 @@
 export const nuts = [ 
     {
+       
         name: 'Almonds',
         img: './assets/almonds.jpeg',
         description:'Almonds can be characterized by three flavor phenotypes: sweet (non-bitter), slightly bitter (or semi-bitter), and bitter.',
+        catagory:'nuts',
         price: '$10 per lbs',
 
     }, 
     {
+         
         name:'Cashew',
         img:'./assets/cashew.jpeg',
         description:'Cashews have a mild flavor and creamy, rich texture, with a similar mouthfeel to peanuts. ',  
+        catagory:'nuts',
         price: '$12 per lbs',
     },
     {
+        
         name:'Pistachio',
         img:'./assets/pistachio.jpeg',
         description:' Pistachio are fresh, piney, terpy and resinous with a slight minty, camphoraceous and spicy nuance.',  
+        catagory:'nuts',
         price: '$15 per lbs',
     },
     {
+         
         name:'Walnuts',
         img:'./assets/walnuts.jpeg', 
         description:'Walnut flavor is, in general, fairly mild, but it has a complexity that includes some tangy and sharp notes. ',  
-        price:'$9 per lbs'
+        catagory:'nuts',
+        price:'$9 per lbs',
     }, 
     {
+         
         name:'Sunflower Seeds',
         img:'./assets/sunflower-seeds.jpeg', 
         description:'Sunflower seeds have a mild, nutty flavor and a firm but tender texture.',  
-        price:'$8 per lbs'
+        catagory:'nuts',
+        price:'$8 per lbs',
     }, 
     
 
