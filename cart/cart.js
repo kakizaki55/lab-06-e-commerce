1df42cde2ee7e7f03529c0331ec0e85eef7dfd34
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-12 13:21:16 -0700

    reding works but i need to figure out the total price because it comes out as NaN

--- cart/cart.js
+++ cart/cart.js
@@ -2,12 +2,29 @@ import { nuts } from '../data/products.js';
 import { cartItems } from '../data/cart-data.js';
 import { findById } from '../data/findbyid.js';
 
+const tableBody = document.getElementById('table-body');
+
 for (let cartItem of cartItems){{
     const itemObject = findById(cartItem.id, nuts);
     console.log(itemObject);
     console.log(itemObject.name);
     console.log(itemObject.price);
     console.log(cartItem.qty);
+
+
+    const tr = document.createElement('tr');
+    const tdName = document.createElement('td');
+    const tdprice = document.createElement('td');
+    const tdqty = document.createElement('td');
+    const tdTotal = document.createElement('td');
+
+    tdName.textContent = itemObject.name;
+    tdprice.textContent = itemObject.price;
+    tdqty.textContent = cartItem.qty;
+    tdTotal.textContent = itemObject.price * cartItem.qty;
+
+    tr.append(tdName, tdprice, tdqty, tdTotal);
+    tableBody.append(tr);
 
 }}
 
