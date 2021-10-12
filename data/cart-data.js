5121fe23ed22ecb8becc198d26e3e374e57fc62f
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-12 11:34:34 -0700

    made some static order data

---
+++ data/cart-data.js
@@ -0,0 +1,14 @@
+export const cartItem = [ 
+    {
+        id: '1', 
+        qty: 2
+    }, 
+    {
+        id:'3', 
+        qty:'2', 
+    }
+    {
+        id:"4", 
+        qty: 1 
+    }
+];
